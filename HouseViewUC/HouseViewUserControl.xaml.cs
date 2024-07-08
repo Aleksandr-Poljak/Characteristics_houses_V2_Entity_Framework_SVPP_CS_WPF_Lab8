267e7c91ae9365fd3937845eaf0af3c322280b16
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-07-08 21:05:06 +0300

    Created custom control for editing House object.  Supports validation of input values.

---
+++ HouseViewUC/HouseViewUserControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using SVPP_CS_WPF_Lab8_Characteristics_houses_Db_V2_Entity_Framework_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SVPP_CS_WPF_Lab8_Characteristics_houses_Db_V2_Entity_Framework_
+{
+    /// <summary>
+    /// Логика взаимодействия для HouseViewUserControl.xaml
+    /// </summary>
+    public partial class HouseViewUserControl : UserControl
+    {
+        private House house;
+        public House House
+        {
+            get => house;
+            set { ViewHouse( ref value); }
+        }
+
+        /// <summary>
+        /// Конструктор для создания нового объекта
+        /// </summary>
+        public HouseViewUserControl()
+        {
+            InitializeComponent();
+            House = new House();
+        }
+
+        /// <summary>
+        /// Конструктор для редактирования существующего объекта.
+        /// </summary>
+        public HouseViewUserControl(ref House house)
+        {
+            InitializeComponent();
+            House = house;
+        }
+
+        /// <summary>
+        /// Устанавливает объект House для редактирования.
+        /// </summary>
+        public void ViewHouse( ref House house)
+        {
+            this.house = house;
+            Grid_Main_HouseView.DataContext = this.house;
+        }
+    }
+}
---
+++ HouseViewUC/NullToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SVPP_CS_WPF_Lab8_Characteristics_houses_Db_V2_Entity_Framework_.HouseViewUC
+{
+    /// <summary>
+    /// Конвертер для обработки null-значений.
+    /// </summary>
+    class NullToStringConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return string.Empty;
+            return value.ToString()!;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string valueStr = (string)value;
+            if (valueStr.Length == 0) return null;
+            return value;
+        }
+    }
+}
