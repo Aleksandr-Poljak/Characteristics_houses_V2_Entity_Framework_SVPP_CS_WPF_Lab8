4e988b191b483cda7f62738a383dc3e0e5f10699
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-07-08 21:05:54 +0300

    Implemented interfaces INotifyPropertyChanged, IDataErrorInfo

--- Models/House.cs
+++ Models/House.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SVPP_CS_WPF_Lab8_Characteristics_houses_Db_V2_Entity_Framework_.Models
 {
-    public  class House
+    public  class House: INotifyPropertyChanged, IDataErrorInfo
     {
         int id = 0;
         string city = string.Empty;
@@ -18,15 +20,152 @@ namespace SVPP_CS_WPF_Lab8_Characteristics_houses_Db_V2_Entity_Framework_.Models
         int? tel = null;
         string? owner = null;
 
-        public int Id { get => id; set => id = value; }
-        public string City { get => city; set => city = value; }
-        public string Street { get => street; set => street = value; }
-        public int Number { get => number; set => number = value; }
-        public int? Flat { get => flat; set => flat = value; }
-        public bool HasElevator { get => hasElevator; set => hasElevator = value; }
-        public int? Floor { get => floor; set => floor = value; }
-        public int? Tel { get => tel; set => tel = value; }
-        public string? Owner { get => owner; set => owner = value; }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public int Id
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+        public string City
+        {
+            get => city;
+            set
+            {
+                city = value;
+                OnPropertyChanged(nameof(City));
+            }
+        }
+        public string Street
+        {
+            get => street;
+            set
+            {
+                street = value;
+                OnPropertyChanged(nameof(Street));
+            }
+        }
+        public int Number
+        {
+            get => number;
+            set
+            {
+                number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+        public int? Flat
+        {
+            get => flat;
+            set
+            {
+                flat = value;
+                OnPropertyChanged(nameof(Flat));
+            }
+        }
+        public bool HasElevator
+        {
+            get => hasElevator;
+            set
+            {
+                hasElevator = value;
+                OnPropertyChanged(nameof(HasElevator));
+            }
+        }
+        public int? Floor
+        {
+            get => floor;
+            set
+            {
+                floor = value;
+                OnPropertyChanged(nameof(Floor));
+            }
+        }
+        public int? Tel
+        {
+            get => tel;
+            set
+            {
+                tel = value;
+                OnPropertyChanged(nameof(Tel));
+            }
+        }
+        public string? Owner
+        {
+            get => owner;
+            set
+            {
+                owner = value;
+                OnPropertyChanged(nameof(Owner));
+            }
+        }
+
+        public string Error => throw new NotImplementedException();
+        private bool notErrorValue = true;
+        public bool NotErrorValue 
+        {
+            get => notErrorValue;
+            private set
+            {
+                if(value != notErrorValue)
+                {
+                    notErrorValue = value;
+                    OnPropertyChanged(nameof(NotErrorValue));
+                }
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = String.Empty;
+                switch (columnName)
+                {
+                    case nameof(Id):
+                        if (Id < 0)
+                            error = "Неверный Id!";
+                        break;
+                    case nameof(City):
+                        if (City.Length > 50 || City.Length == 0)
+                            error = "Неверная длина!";
+                        break;
+                    case nameof(Street):
+                        if (Street.Length > 50 || Street.Length == 0)
+                            error = "Неверная длина!";
+                        break;
+                    case nameof(Number):
+                        if (Number < 1)
+                            error = "Неверно указан номер дома";
+                        break;
+                    case nameof(Flat):
+                        if (Flat is int f && f < 1)
+                            error = "Неверно указан номер квартиры";
+                        break;
+                    case nameof(Floor):
+                        if (Floor is int fl && fl < 0)
+                            error = "Неверно указан этаж";
+                        break;
+                    case nameof(Owner):
+                        if (Owner is string && Owner.Length > 50)
+                            error = "Слишком длинное поле!";
+                        break;
+
+                }
+                if (error == string.Empty) NotErrorValue = true;
+                else { NotErrorValue = false; }
+                return error;
+            }
+        }
 
         public House() { }
 
